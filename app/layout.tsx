0f2fc5d4e3e0d0b45e89cdfe6615a987e2388772
thh-source <savedatathh@gmail.com>
2026-09-11 21:08:01 +0700

    Mount supplier picker search enhancer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import SupplierPickerEnhancer from "./SupplierPickerEnhancer";
 import "./globals.css";
 import "./rebuild.css";
 
@@ -41,6 +42,7 @@ export default function RootLayout({
     <html lang="vi">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <SupplierPickerEnhancer />
       </body>
     </html>
   );
